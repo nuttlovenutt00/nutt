21300b527753a13cc2495698ea963eeedbcd5f3c
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-20 12:13:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
 <?php
-$servername = "37.59.55.185";
-$username = "Z01XVlWSlA";
-$password = "ogqvLgVKmd";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=Z01XVlWSlA", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+$servername = "37.59.55.185";
+  $username = "Z01XVlWSlA";
+  $password = "ogqvLgVKmd";
+  $dbname = "Z01XVlWSlA";
+  $mysql = new mysqli($servername, $username, $password, $dbname);
+  mysqli_set_charset($mysql, "utf8");
+
+  if ($mysql->connect_error){
+  $errorcode = $mysql->connect_error;
+  print("MySQL(Connection)> ".$errorcode);
+  }
+
+  $mysql->query("INSERT INTO `user`(`id`, `pass`, `name`,tel) VALUES ('111','222','333','333')");
+
+  $getUser = $mysql->query("SELECT * FROM `user` ");
+ 
+    while($row = $getUser->fetch_assoc()){
+     echo  $row['id']."<br>";
     }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
+ 
+ 
+
 ?>
