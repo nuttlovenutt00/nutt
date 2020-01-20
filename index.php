484d3cd63d0adb9257e0b0ccf88781751473618f
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-20 11:51:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
- echo "5555";
+$servername = "37.59.55.185";
+$username = "Z01XVlWSlA";
+$password = "ogqvLgVKmd";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=Z01XVlWSlA", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 ?>
