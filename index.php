83408dee1da3aa9b5b4e16e033933093b1bd61f1
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-20 13:24:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -45,23 +45,23 @@
 
  	$replyText1= array(
  	"type"=> "template",
-  "altText"=> "this is a confirm template",
-  "template"=> {
-      "type"=> "confirm",
-      "text"=> "Are you sure?",
-      "actions": [
-          {
-            "type"=> "message",
-            "label"=> "Yes",
-            "text"=> "yes"
-          },
-          {
-            "type"=> "message",
-            "label"=> "No",
-            "text"=> "no"
-          }
-      ]
-  }
+ 	"altText"=> "this is a confirm template",
+ 	 "template"=> 	(
+     				 "type"=> "confirm",
+     				 "text"=> "Are you sure?",
+     				 "actions": (
+							        (
+							         	"type"=> "message",
+							            "label"=> "Yes",
+							            "text"=> "yes"
+							        ),
+							        (
+							            "type"=> "message",
+							            "label"=> "No",
+							            "text"=> "no"
+							         )
+				      			)
+				  	)
 	);
 
 	
