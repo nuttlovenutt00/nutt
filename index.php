d14c29408d2023b3b1668f360cb66d9643731aa2
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-20 23:05:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -42,23 +42,72 @@
   }else{
      $replyText["text"] = "กรุณาพิม สั่งอาหาร";
   }
-   $replyText1["type"] = "template";
-  $replyText1["altText"] = "this is a confirm template";
-  $replyText1["template"] = [
-    "type" => "confirm",
-        "text" => "Are you sure?",
-        "actions" => [
-     [
-        "type" => "message",
-        "label" => "Yes",
-        "text" => "yes"
+   $replyText1=[
+    "type"=> "template",
+  "altText"=> "this is a carousel template",
+  "template"=> [
+      "type"=> "carousel",
+      "columns"=> [
+          [
+            "thumbnailImageUrl"=> "https://example.com/bot/images/item1.jpg",
+            "imageBackgroundColor"=> "#FFFFFF",
+            "title"=> "this is menu",
+            "text"=> "description",
+            "defaultAction"=> [
+                "type"=> "uri",
+                "label"=> "View detail",
+                "uri"=> "http://example.com/page/123"
+            ],
+            "actions"=> [
+                [
+                    "type"=> "postback",
+                    "label"=> "Buy",
+                    "text"=> "action=buy&itemid=111"
+                ],
+                [
+                    "type"=> "postback",
+                    "label"=> "Add to cart",
+                    "text"=> "action=add&itemid=111"
+                ],
+                [
+                    "type"=> "uri",
+                    "label"=> "View detail",
+                    "text"=> "http://example.com/page/111"
+                ]
+            ]
+          ],
+          [
+            "thumbnailImageUrl"=> "https://example.com/bot/images/item2.jpg",
+            "imageBackgroundColor"=>"#000000",
+            "title"=> "this is menu",
+            "text"=> "description",
+            "defaultAction"=> [
+                "type"=> "uri",
+                "label"=> "View detail",
+                "uri"=> "http://example.com/page/222"
+            ],
+            "actions"=> [
+                [
+                    "type"=> "postback",
+                    "label"=> "Buy",
+                    "text"=> "action=buy&itemid=222"
+                ],
+                [
+                    "type"=> "postback",
+                    "label"=> "Add to cart",
+                    "text"=> "action=add&itemid=222"
+                ],
+                [
+                    "type"=> "uri",
+                    "label"=> "View detail",
+                    "text"=> "http://example.com/page/222"
+                ]
+            ]
+          ]
       ],
-      [
-        "type" => "message",
-        "label" => "No",
-        "text" => "no"
-      ]
-       ]
+      "imageAspectRatio"=> "rectangle",
+      "imageSize"=> "cover"
+  ]
   ];
   
 
