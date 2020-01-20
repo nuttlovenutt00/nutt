31ca9d5b3c3558e4e13a527806fdcaf43d6202ea
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-20 11:36:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-  echo "Hello";
+$servername = "http://viscctv.dyndns.org:8088/phpmyadmin/";
+$username = "root";
+$password = "root36698";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=hutcomp", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 ?>
