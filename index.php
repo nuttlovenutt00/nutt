64605e6a282e699ffa452991ea40b9bef7fd220a
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-21 22:38:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -103,6 +103,146 @@
                     "uri"=>  "http://example.com/page/222"
                 ]
             ]
+          ],
+          [
+            "thumbnailImageUrl"=> "https://example.com/bot/images/item2.jpg",
+            "imageBackgroundColor"=>  "#000000",
+            "title"=>  "this is menu",
+            "text"=>  "description",
+            "defaultAction"=>  [
+                "type"=> "uri",
+                "label"=> "View detail",
+                "uri"=>  "http://example.com/page/222"
+            ],
+            "actions"=>  [
+                [
+                    "type"=> "postback",
+                    "label"=>  "Buy",
+                    "data"=>  "action=buy&itemid=222"
+                ],
+                [
+                    "type"=>  "postback",
+                    "label"=>  "Add to cart",
+                    "data"=>  "action=add&itemid=222"
+                ],
+                [
+                    "type"=>  "uri",
+                    "label"=>  "View detail",
+                    "uri"=>  "http://example.com/page/222"
+                ]
+            ]
+          ],
+          [
+            "thumbnailImageUrl"=> "https://example.com/bot/images/item2.jpg",
+            "imageBackgroundColor"=>  "#000000",
+            "title"=>  "this is menu",
+            "text"=>  "description",
+            "defaultAction"=>  [
+                "type"=> "uri",
+                "label"=> "View detail",
+                "uri"=>  "http://example.com/page/222"
+            ],
+            "actions"=>  [
+                [
+                    "type"=> "postback",
+                    "label"=>  "Buy",
+                    "data"=>  "action=buy&itemid=222"
+                ],
+                [
+                    "type"=>  "postback",
+                    "label"=>  "Add to cart",
+                    "data"=>  "action=add&itemid=222"
+                ],
+                [
+                    "type"=>  "uri",
+                    "label"=>  "View detail",
+                    "uri"=>  "http://example.com/page/222"
+                ]
+            ]
+          ],
+          [
+            "thumbnailImageUrl"=> "https://example.com/bot/images/item2.jpg",
+            "imageBackgroundColor"=>  "#000000",
+            "title"=>  "this is menu",
+            "text"=>  "description",
+            "defaultAction"=>  [
+                "type"=> "uri",
+                "label"=> "View detail",
+                "uri"=>  "http://example.com/page/222"
+            ],
+            "actions"=>  [
+                [
+                    "type"=> "postback",
+                    "label"=>  "Buy",
+                    "data"=>  "action=buy&itemid=222"
+                ],
+                [
+                    "type"=>  "postback",
+                    "label"=>  "Add to cart",
+                    "data"=>  "action=add&itemid=222"
+                ],
+                [
+                    "type"=>  "uri",
+                    "label"=>  "View detail",
+                    "uri"=>  "http://example.com/page/222"
+                ]
+            ]
+          ],
+          [
+            "thumbnailImageUrl"=> "https://example.com/bot/images/item2.jpg",
+            "imageBackgroundColor"=>  "#000000",
+            "title"=>  "this is menu",
+            "text"=>  "description",
+            "defaultAction"=>  [
+                "type"=> "uri",
+                "label"=> "View detail",
+                "uri"=>  "http://example.com/page/222"
+            ],
+            "actions"=>  [
+                [
+                    "type"=> "postback",
+                    "label"=>  "Buy",
+                    "data"=>  "action=buy&itemid=222"
+                ],
+                [
+                    "type"=>  "postback",
+                    "label"=>  "Add to cart",
+                    "data"=>  "action=add&itemid=222"
+                ],
+                [
+                    "type"=>  "uri",
+                    "label"=>  "View detail",
+                    "uri"=>  "http://example.com/page/222"
+                ]
+            ]
+          ],
+          [
+            "thumbnailImageUrl"=> "https://example.com/bot/images/item2.jpg",
+            "imageBackgroundColor"=>  "#000000",
+            "title"=>  "this is menu",
+            "text"=>  "description",
+            "defaultAction"=>  [
+                "type"=> "uri",
+                "label"=> "View detail",
+                "uri"=>  "http://example.com/page/222"
+            ],
+            "actions"=>  [
+                [
+                    "type"=> "postback",
+                    "label"=>  "Buy",
+                    "data"=>  "action=buy&itemid=222"
+                ],
+                [
+                    "type"=>  "postback",
+                    "label"=>  "Add to cart",
+                    "data"=>  "action=add&itemid=222"
+                ],
+                [
+                    "type"=>  "uri",
+                    "label"=>  "View detail",
+                    "uri"=>  "http://example.com/page/222"
+                ]
+            ]
           ]
       ],
       "imageAspectRatio"=>  "rectangle",
