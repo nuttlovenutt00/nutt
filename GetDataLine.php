c0bf4bc0d2770e7936b40977357710fbe9f62eae
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-23 21:00:43 +0700

    Create GetDataLine.php

---
+++ GetDataLine.php
@@ -0,0 +1,6 @@
+  $LINEData = file_get_contents('php://input');
+  $jsonData = json_decode($LINEData,true);
+  $replyToken = $jsonData["events"][0]["replyToken"];
+  $userID = $jsonData["events"][0]["source"]["userId"];
+  $text = $jsonData["events"][0]["message"]["text"];
+  $timestamp = $jsonData["events"][0]["timestamp"];
