207e61543207a4e482cf3eaa62f933671a5a88d4
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-23 21:08:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
 <?php
 
   include("GetDataLine.php");
+  include("ConnectSql.php.php");
 
 
-  $servername = "37.59.55.185";
-  $username = "Z01XVlWSlA";
-  $password = "ogqvLgVKmd";
-  $dbname = "Z01XVlWSlA";
-  $mysql = new mysqli($servername, $username, $password, $dbname);
-  mysqli_set_charset($mysql, "utf8");
-  if ($mysql->connect_error){
-  $errorcode = $mysql->connect_error;
-  print("MySQL(Connection)> ".$errorcode);
-  }
  
-    $mysql->query("INSERT INTO `LOG`(`UserID`, `Text`, `Timestamp`) VALUES ('$userID','$text','$timestamp')");
+ $mysql->query("INSERT INTO `LOG`(`UserID`, `Text`, `Timestamp`) VALUES ('$userID','$text','$timestamp')");
 
    $replyText["type"] = "text";
 
@@ -136,17 +127,32 @@ $b="";
           $num++;
     }
 
+$replyText1= [ 
+  "type"=> "template",
+  "altText"=>  "this is a carousel template",
+  "template"=>  [
+      "type"=>  "carousel",
+      "columns"=>  
 
+      
+      $a2
+    
+         
+      ,
+      "imageAspectRatio"=>  "rectangle",
+      "imageSize"=>  "cover"
+  ]
+  ];
  
 
 
-include("connn.php");
+
 
 
   
 
   
- 
+ $replyJson["messages"][0] = $replyText;
   
   if($text=="เมนูกาแฟ")
   {
