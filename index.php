706ec15f0f4bf2e38f02b4fb57c7665df3277d34
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-23 21:38:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,8 +35,8 @@
   }
 
   //บันทึก Log ไฟล์
-  $mysql->query("INSERT INTO 'LOG'('UserID','replyToken', 'Text', 'Timestamp', 'datetime') 
-    VALUES ('$userID','111','$text','$timestamp','333')");
+  $mysql->query("INSERT INTO 'LOG'('UserID','Text','Timestamp') 
+    VALUES ('$userID','$text','$timestamp')");
 
   $replyText["type"] = "text";
 
