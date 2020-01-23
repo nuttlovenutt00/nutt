f72d692aa07ba879013bdb3e3fdb3e62e115f29a
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-23 21:13:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
 
   include("GetDataLine.php");
-  include("ConnectSql.php.php");
 
 
+  $servername = "37.59.55.185";
+  $username = "Z01XVlWSlA";
+  $password = "ogqvLgVKmd";
+  $dbname = "Z01XVlWSlA";
+  $mysql = new mysqli($servername, $username, $password, $dbname);
+  mysqli_set_charset($mysql, "utf8");
+  if ($mysql->connect_error){
+  $errorcode = $mysql->connect_error;
+  print("MySQL(Connection)> ".$errorcode);
+  }
  
  $mysql->query("INSERT INTO `LOG`(`UserID`, `Text`, `Timestamp`) VALUES ('$userID','$text','$timestamp')");
 
