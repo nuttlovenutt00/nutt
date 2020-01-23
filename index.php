bb9521ed4453f0327f451f24fdd129faa5308c65
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-23 16:53:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ $b="";
       while($row2 = $result2->fetch_assoc()) 
         {
       
-            $a[$numm]= $row2['m_name']." ".$row2['m_price'];
+            $a[$numm]= $row2['m_name']."&nbsp;".$row2['m_price'];
            $numm++;
         }
         
