bb80c32dd0a734087e727fa4b00bde9405797ebe
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-23 20:57:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -156,22 +156,7 @@ $b="";
     }
 
 
-  $replyText1= [ 
-  "type"=> "template",
-  "altText"=>  "this is a carousel template",
-  "template"=>  [
-      "type"=>  "carousel",
-      "columns"=>  
-
-      
-      $a2
-    
-         
-      ,
-      "imageAspectRatio"=>  "rectangle",
-      "imageSize"=>  "cover"
-  ]
-  ];
+ 
 
 
 include("connn.php");
