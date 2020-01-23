7815c7d89e0e59a73ece4109e574776b64adf72c
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-23 21:03:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 
-
-  $LINEData = file_get_contents('php://input');
-  $jsonData = json_decode($LINEData,true);
-  $replyToken = $jsonData["events"][0]["replyToken"];
-  $userID = $jsonData["events"][0]["source"]["userId"];
-  $text = $jsonData["events"][0]["message"]["text"];
-  $timestamp = $jsonData["events"][0]["timestamp"];
+  include("GetDataLine.php");
 
 
   $servername = "37.59.55.185";
@@ -19,20 +13,7 @@
   $errorcode = $mysql->connect_error;
   print("MySQL(Connection)> ".$errorcode);
   }
-  function sendMessage($replyJson, $sendInfo){
-          $ch = curl_init($sendInfo["URL"]);
-          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-          curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-          curl_setopt($ch, CURLOPT_POST, true);
-          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-              'Content-Type: application/json',
-              'Authorization: Bearer ' . $sendInfo["AccessToken"])
-              );
-          curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
-          $result = curl_exec($ch);
-          curl_close($ch);
-    return $result;
-  }
+ 
     $mysql->query("INSERT INTO `LOG`(`UserID`, `Text`, `Timestamp`) VALUES ('$userID','$text','$timestamp')");
 
    $replyText["type"] = "text";
@@ -165,19 +146,12 @@ include("connn.php");
   
 
   
-  $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
-  $lineData['AccessToken'] = "0EhBTTseT51jUDZTB2ExoXM+4VM59TybE8WoW6GdG7I9ugLQyQssBVyKuWw18GgvhVOXYLtJCbAwnamRdP10iFyFkpSIdlgskfDHONLWlJ/f9MB9IitlaOHZzIyGxDZgrDLiX+XXp/BOq+4SjJZe7AdB04t89/1O/w1cDnyilFU=";
-  $replyJson["replyToken"] = $replyToken;
-  $replyJson["messages"][0] = $replyText;
+ 
   
   if($text=="เมนูกาแฟ")
   {
     $replyJson["messages"][0] = $replyText1;
   }
+
    
-  
-  $encodeJson = json_encode($replyJson);
-  $results = sendMessage($encodeJson,$lineData);
-  echo $results;
-  http_response_code(200);
 ?>
