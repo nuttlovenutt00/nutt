16cd18793ec8b662fa4aca26772d75ea2d12b529
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-23 22:36:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
   //ตั้งค่าการตอบ-รับข้อความ
   $type_product="";
 
-  if(strpos($text, "H") !== FALSE)
+  if(strpos($text, "H") !== FALSE || strpos($text, "C") !== FALSE || strpos($text, "S") !== FALSE)
   {
       $replyText["text"] = "ระบบได้ทำการบันทึก Order ของท่านแล้วค่ะ";
   }elseif($text=="เมนูกาแฟ")
