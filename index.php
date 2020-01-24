d0768329538718dc83413c10369ecc41f87a3d28
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-24 23:03:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
 
   //ตั้งค่าการตอบ-รับข้อความ
   $type_product="";
-  if(strpos($text, "MP") == "0" )
+  if(strpos($text, "MP") !== FALSE )
   { 
 
     $sql_sdrt = "Select * from  OrderTemp  where ortUserId='$userID' order by orAutoId DESC";
