4f6cf5642dfc7472cf37f8593d6ef3c0cbfd57ea
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-01-29 16:16:14 +0700

    Add files via upload

---
+++ t.php
@@ -0,0 +1,40 @@
+<?php
+
+ //รับค่าจาก line
+  $LINEData = file_get_contents('php://input');
+  $jsonData = json_decode($LINEData,true);
+
+
+
+
+   //ฟังก์ชั่นการส่งข้อมูลไปหา Line
+  $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
+  $lineData['AccessToken'] = "0EhBTTseT51jUDZTB2ExoXM+4VM59TybE8WoW6GdG7I9ugLQyQssBVyKuWw18GgvhVOXYLtJCbAwnamRdP10iFyFkpSIdlgskfDHONLWlJ/f9MB9IitlaOHZzIyGxDZgrDLiX+XXp/BOq+4SjJZe7AdB04t89/1O/w1cDnyilFU=";
+  $replyJson["replyToken"] = "cb40a508e6754433bdb38fa62d9719a1";
+
+  function sendMessage($replyJson, $sendInfo){
+          $ch = curl_init($sendInfo["URL"]);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+          curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+          curl_setopt($ch, CURLOPT_POST, true);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+              'Content-Type: application/json',
+              'Authorization: Bearer ' . $sendInfo["AccessToken"])
+              );
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
+          $result = curl_exec($ch);
+          curl_close($ch);
+    return $result;
+  }
+
+$replyText_sp["type"] = "text";
+          $replyText_sp["text"] = "เซม";
+          $replyJson["messages"][0] = $replyText_sp;
+
+
+   //ส่งค่าทั้งหมดกลับไปหา Line
+  $encodeJson = json_encode($replyJson);
+  $results = sendMessage($encodeJson,$lineData);
+  echo $results;
+  http_response_code(200);
+?>
