f3dbcf9e842606417290c80a4b0d837c20bb28e1
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-02-03 16:26:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -835,7 +835,7 @@
                                           ],
                                           [
                                             "type"=> "text",
-                                            "text"=> $id_Q,
+                                            "text"=> "nn",
                                             "size"=> "4xl",
                                             "align"=> "center",
                                             "weight"=> "bold",
