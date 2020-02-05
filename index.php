c0cd2b195eaa7fb7666840cb1a63838c17cc2ca3
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-02-05 21:06:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -247,7 +247,7 @@
                 $textpro_pass[$zz]=[
                       "type"=> "text",
                                   "text"=> $textt,
-                                  "size"=> "xxs",
+                                  "size"=> "xs",
                                   "color"=> "#000000"
                     ];
               }
@@ -255,7 +255,7 @@
               $textpro_pass[0]=[
                       "type"=> "text",
                                   "text"=> "ไม่มีการรับออร์เดอร์",
-                                  "size"=> "xxs",
+                                  "size"=> "xs",
                                   "color"=> "#000000"
                     ];
             }
